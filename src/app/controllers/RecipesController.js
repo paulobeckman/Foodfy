436b2bcd4810e2c42e9b02c1fc4a143cfa1dc4db
paulobeckman <beckmam.paulo@hotmail.com>
2021-04-27 12:37:27 -0300

    creating about and recipes screen

---
+++ src/app/controllers/RecipesController.js
@@ -0,0 +1,5 @@
+module.exports = {
+    index(req, res){
+        return res.render("site/recipes/index")
+    }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const routes = express.Router()
 
-const Layout = require('../app/controllers/HomeController')
+const Home = require('../app/controllers/HomeController')
+const Recipes = require('../app/controllers/RecipesController')
 
-routes.get('/', Layout.index)
+routes.get('/', Home.index)
+routes.get('/about', function(req, res){
+    return res.render("site/about/index")
+})
+routes.get('/recipes', Recipes.index)
 
 module.exports = routes
