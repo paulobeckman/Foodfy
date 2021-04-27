5eac38b4c5e66aa8e71f18c0658f95f31d1f6b79
paulobeckman <beckmam.paulo@hotmail.com>
2021-04-27 17:56:20 -0300

    creating bosses screen

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const routes = express.Router()
 
 const Home = require('../app/controllers/HomeController')
 const Recipes = require('../app/controllers/RecipesController')
+const Bosses = require('../app/controllers/BossesController')
 
 routes.get('/', Home.index)
 routes.get('/about', function(req, res){
     return res.render("site/about/index")
 })
 routes.get('/recipes', Recipes.index)
+routes.get('/bosses', Bosses.index)
 
 module.exports = routes
