159940d461288f4710b980deec928dc9c1e57a5d
paulobeckman <beckmam.paulo@hotmail.com>
2021-05-15 20:52:43 -0300

    finish

---
+++ src/app/controllers/user/SessionController.js
@@ -0,0 +1,11 @@
+module.exports = {
+    loginForm(req, res){
+        return res.render("session/login")
+    },
+    forgotForm(req, res){
+        return res.render("session/forgot-password")
+    },
+    resetForm(req, res){
+        return res.render("session/password-reset")
+    }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const routes = express.Router()
 
 const site = require('./site')
 const admin = require('./admin')
+const users = require('./users')
 
 routes.use('/', site)
 routes.use('/admin', admin)
+routes.use('/users', users)
 
 module.exports = routes
