d624977c329d260f0e5fb69837d53708c6f976b7
paulobeckman <beckmam.paulo@hotmail.com>
2021-05-17 11:24:38 -0300

    finalized user and profile update screen

--- src/app/controllers/user/UserController.js
+++ src/app/controllers/user/UserController.js
@@ -1,5 +1,11 @@
 module.exports = {
     list(req, res){
         return res.render("admin/users/index")
+    },
+    create(req, res){
+        return res.render("admin/users/create")
+    },
+    edit(req, res){
+        return res.render("admin/users/edit")
     }
 }
